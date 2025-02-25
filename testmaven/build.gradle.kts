1da19bae2b301703779b7d56d419dbaa258d639b
beom.93 <beom.93@kakaomobility.com>
2025-02-25 13:56:38 +0900

    public maven 셋업완료

--- testmaven/build.gradle.kts
+++ testmaven/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 android {
@@ -37,4 +40,38 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("io.github.goddb123", "kmtest-testmaven", "1.0.0")
+
+    pom {
+        name = "kmtestTestmaven"
+        description = "maven public test"
+        url = "https://github.com/godDB123/TestMavenPublic"
+        inceptionYear = "2025"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "godDB123"
+                name = "godDB123"
+                url = "https://github.com/godDB123"
+            }
+        }
+
+        scm {
+            connection = "scm:git:github.com/godDB123/TestMavenPublic.git"
+            developerConnection.set("scm:git:ssh://github.com:godDB123/TestMavenPublic.git")
+            url = "https://github.com/godDB123/TestMavenPublic/master"
+        }
+    }
 }
